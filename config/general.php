402de5d17cf3dc0c76862a759eb1d37a15f46a8b
Sean Prunka <sprunka@gmail.com>
2016-05-22 13:19:47 -0400

    typo

--- config/general.php
+++ config/general.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * If you have access to command line, setup the enviroment var ENV to equal your current enviornment:
+ * If you have access to command line, setup the environment var ENV to equal your current enviornnment:
  * dev, stage, prod
  * If you do not have access to environment vars, or
  * if you do not set a value for ENV the app will run in production mode.
