ba78e3a3fe7ec911473210d6151e22d07a241361
Sean Prunka <sprunka@gmail.com>
2016-05-29 19:05:52 -0400

    Just reformatting and code style changes.

--- src/AbstractRoute.php
+++ src/AbstractRoute.php
@@ -1,23 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sean
- * Date: 2016.05.08
- * Time: 22:12
- */
-
 namespace CybernetAPI;
 
+use Slim\Http\Request;
+use Slim\Http\Response;
 
+/**
+ * Class AbstractRoute
+ * @package CybernetAPI
+ */
 abstract class AbstractRoute
 {
+    /**
+     * @var array
+     */
     protected $help = [];
+
+    /**
+     * AbstractRoute constructor.
+     * @param $container
+     */
     public function __construct($container)
     {
         $this->container = $container;
     }
+
+    /**
+     * @return object
+     */
     public function getHelp()
     {
         return (object)$this->help;
     }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return mixed
+     */
+    abstract public function __invoke(Request $request, Response $response);
 }
